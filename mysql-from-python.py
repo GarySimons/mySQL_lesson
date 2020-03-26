eb41c27113dbee998f494b3b71bb34c7cd8d9dbe
GarySimons <garycsimons@gmail.com>
2020-03-26 09:02:20 +0000

    Adding a Table and data in SQL with Python

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,21 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
-# Modify this variable if running on other environment
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = ("bob", 21, "1990-02-06 23:04:56")
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        connection.commit()
 finally:
-    # Close the connection. regardless of whether the above was successful
     connection.close()
